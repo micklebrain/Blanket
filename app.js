256fe3ba703e589e24de75d739b1d361b53170b0
Nathan Nguyen <nathan.nguyen@accenture.com>
2019-09-09 14:54:31 -0400

    Simple Request and response for endpoint

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+function parseLocations(locations) {
+    locations.forEach(location => {
+        console.log(location);
+    });
+}
+
+var cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
+
 // Initialize the app.
 const server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
@@ -11,4 +21,13 @@ const server = app.listen(process.env.PORT || 8080, function () {
 
 app.get('/health', (req, res) => res.send('Application is healthy'));
 
-app.get('/Blanket/Locations', (req, res) => res.send('Return list of closest endpoints'));
+app.get('/Blanket/Locations', (req, res) => {
+
+    // Validate Locations input 
+
+    // Find closest location for each 
+    parseLocations(req.body.locations);
+
+    // console.log(req.body.locations);
+    res.send(req.body.locations);
+});
