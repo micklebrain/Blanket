5b7afa28f9fcfb95a3f4e3af39b900b0f0e5ee30
Nathan Nguyen <nathan.nguyen@accenture.com>
2019-09-09 17:30:56 -0400

    Dependencies

--- app.js
+++ app.js
@@ -71,7 +71,6 @@ function parseLocations(locations) {
     });
 }
 
-
 // Initialize the app.
 const server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
